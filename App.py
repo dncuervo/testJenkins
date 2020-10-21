f58f57d168efa037fa7db765de47bef7d87f3a97
dncuervo <70612016+dncuervo@users.noreply.github.com>
2020-10-21 08:38:53 -0500

    Update App.py

--- App.py
+++ App.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def base():
-    return Response(response=json.dumps({"Status": "UP2"}),
+    return Response(response=json.dumps({"Status": "UP3"}),
                     status=200,
                     mimetype='application/json')
   
